8e653e53085c9a39a505ccf9931d81c1876134f2
Yohanes Setiawan <yohanes@daruma.co.id>
2018-05-25 16:41:22 +0700

    finish project

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,47 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import {TryAgainButton} from './style/style';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import {TryAgainButton, Advertisement} from './style/style';
 
 class App extends Component {
   state = {
-    passwords: []
+    passwords: [1,2,3,4,4]
   }
 
   getPasswords = () => {
-    axios.get('http://localhost:7000/api/passwords')
+    axios.get('api/passwords')
       .then(res => this.setState({passwords: res.data}))
         .catch(err => console.log(err));
+  }
+
+  componentDidMount() {
+    this.getPasswords()
   }
   
   render() {
     const {passwords} = this.state;
     
-    let render = <div>
+    let render = <div style={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", padding:"5%"}}>
       <h1 style={{fontWeight:"normal", fontSize:"42px"}}>No passwords :</h1>
       <TryAgainButton onClick={this.getPasswords}>Try Again?</TryAgainButton>
     </div>
     
     if (passwords && passwords.length > 0) {
-      render = <div>
-        <h1 style={{fontWeight:"normal", fontSize:"42px"}}>{passwords.length} Passwords.</h1>
+      render = <div style={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", padding:"5%"}}>
+        <h1 style={{fontWeight:"normal", fontSize:"44px"}}>{passwords.length} Passwords.</h1>
+        {
+          window.innerWidth <= 480 ?
+          <Advertisement>Create password like <br />never same before...</Advertisement>        
+          : 
+        <Advertisement>Create password like never same before...</Advertisement>                  
+        }
         <ul style={{listStyle: "none", padding:"0", fontSize:"32px", marginBottom:"2em"}}>
           {
-            passwords.map((password, index) => <li key={index}>{password}</li>)
+            passwords.map((password, index) => <CopyToClipboard key={index} text={password}><li style={{paddingBottom: "3px", cursor:"pointer"}}onClick={() => alert('copied!')}>{password}</li></CopyToClipboard>)
           }
         </ul>
-        <TryAgainButton onClick={this.getPasswords}>Get More</TryAgainButton>
+        <TryAgainButton onClick={this.getPasswords}>Generate</TryAgainButton>
+        <Advertisement>Click hash to copy!</Advertisement>
       </div>
     }
 
--- client/src/style/style.js
+++ client/src/style/style.js
@@ -8,11 +8,25 @@ export const TryAgainButton = styled.div`
   padding: 10px 25px;
   transition: .3s ease-in-out;
 
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+
   &:hover {
     background-color: #fdd836;
+    cursor: pointer;
   }
 
   &:active {
     background-color: #ffefa9;
   }
+`
+
+export const Advertisement = styled.span`
+  margin-top: 10px;
+  font-size: 16px;
+  font-family: "Courier New", monospace;
 `
